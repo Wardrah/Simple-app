6a5e83d1cf77a9642a484135ef9baa5dd580557a
Wardrah <wyattleteef@gmail.com>
2021-11-29 11:04:37 -0500

    Initial Commit

---
+++ Simple app/Simple_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_appApp.swift
+//  Simple app
+//
+//  Created by Leteef Wyatt on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Simple_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
